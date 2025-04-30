f5dd89d15f80c7c7a17cb258148b342f484a37e4
MerrikM <merrikm.pl03@gmail.com>
2025-04-30 23:12:20 +0500

    update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"golang.org/x/net/context"
 	"log"
 	"net/http"
+	"notificationservice/internal/WebSocket"
 	"notificationservice/internal/config"
 )
 
+var brokers []string = []string{"localhost:9093"}
+var topics []string = []string{"user-event"}
+var topic = "user-event"
+var groupID = "user-notifier-group"
+
 func main() {
-	cfg, err := config.LoadConfig("config.yaml")
+
+	go WebSocket.StartWebSocketServer("8089")
+
+	// Создаём кастомную ConsumerGroup
+	consumerGroup, err := config.NewCustomConsumerGroup(brokers, groupID, topics)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalf("Ошибка инициализации ConsumerGroup: %v", err)
 	}
 
-	http.HandleFunc("/health", healthCheckHandler)
-	http.HandleFunc("/notify", notifyHandler)
+	context, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	log.Println("Сервер запущен на порту: " + cfg.Server.Port)
-	err = http.ListenAndServe(":"+cfg.Server.Port, nil)
-	if err != nil {
-		fmt.Println("Ошибка запуска сервера: ", err)
-	}
+	// Запуск Kafka consumer
+	go func() {
+		for {
+			select {
+			case <-context.Done():
+				log.Println("Завершение работы consumer...")
+				return
+			default:
+				log.Println("Начинаем потребление сообщений...")
+				// Вызываем Consume, который в свою очередь вызывает ConsumeClaim
+				err := consumerGroup.Group.Consume(context, consumerGroup.Topics, consumerGroup)
+				if err != nil {
+					log.Printf("Ошибка Consume(): %v", err)
+				}
+			}
+		}
+	}()
+
+	select {}
 }
 
 func healthCheckHandler(writer http.ResponseWriter, request *http.Request) {
